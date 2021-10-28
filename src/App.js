eb83e7cf7d28e4af2af8b90000367ddf971ab120
franklyg <f.gisonni@gmail.com>
2021-10-28 00:24:20 -0400

    Upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import leafs from './leafs-logo.png'
 import './App.css';
+import Score from './components/Score';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={leafs} className="logo"/>
+      <Score />
     </div>
   );
 }
---
+++ src/components/Score.js
@@ -0,0 +1,89 @@
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+
+function Score(){
+
+    const [score, setScore] = useState([]);
+    var responses = [
+        "Nope",
+        "F*ck my life",
+        "Triple D is probably on.",
+        "WTF",
+        "LOL",
+        "I can't take this",
+        "Why am I watching this?"
+    ]
+    useEffect(() => {
+        fetch('https://nhl-score-api.herokuapp.com/api/scores/latest')
+        .then(results => results.json())
+        .then(data => {
+            setScore(data.games)
+        });
+    }, []);
+
+    function responseRender(){
+        var randomNumber = Math.floor(Math.random()*responses.length);
+        return responses[randomNumber];
+    }
+
+    return(
+        <div className="component-score">
+            <div class="message">
+                {
+                    score.map((e, i) => {
+                        if(e.scores['TOR']) {
+                        var scoreItem = Object.entries(e.scores)
+                        if(e.status.state == 'FINAL'){
+                            return(
+                                <div>
+                                    {
+                                        scoreItem[0][0] == 'TOR' && scoreItem[0][1] > scoreItem[1][1] // Winning at Home
+                                        || 
+                                        scoreItem[1][0] == 'TOR' && scoreItem[1][1] > scoreItem[0][1] ? // Winning Away
+                                        <p>We Won.</p>
+                                        :
+                                        <p>Onto the next one.</p>
+                                    }
+                                </div>
+                            )
+                        }else{
+                            return(
+                                <div>
+                                    {
+                                        scoreItem[0][0] == 'TOR' && scoreItem[0][1] > scoreItem[1][1] // Winning at Home
+                                        || 
+                                        scoreItem[1][0] == 'TOR' && scoreItem[1][1] > scoreItem[0][1] ? // Winning Away
+                                        <p>We're winning.</p>
+                                        :
+                                        <p>{responseRender()}</p>
+                                    }
+                                </div>
+                            )
+                        }
+                        }
+                    })
+                }
+            </div>
+            <div class="score">
+            {
+                score.map((e,i) => {
+                    return(
+                        e.scores['TOR'] ?
+                        <React.Fragment key={i}>
+                            {
+                                e.scores['TOR'] ?
+                                <p>{JSON.stringify(e.scores).replace(/[{}:"",overtime,true]/g, ' ')}</p>
+                                :
+                                <p>Sorry</p>
+                            }
+                        </React.Fragment>
+                        :
+                        null
+                    )
+                })
+            }
+            </div>
+        </div>
+    )
+}
+export default Score;
