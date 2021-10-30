8a56acfdf9b01c6702b7aeed57d7f76a1d90437e
franklyg <f.gisonni@gmail.com>
2021-10-30 02:44:40 -0400

    Updated packages. Google Analytics added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import leafs from './leafs-logo.png'
 import './App.css';
 import Score from './components/Score';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-211632738-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 function App() {
   return (
     <div className="App">
